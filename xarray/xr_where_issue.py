5de8dc2b9c86437e15d5a4905d5a10de82edb7b0
raybellwaves <rjbell1987@gmail.com>
2017-04-22 22:27:33 -0400

    Update xr_where_issue.py

--- xarray/xr_where_issue.py
+++ xarray/xr_where_issue.py
@@ -1,1 +1,21 @@
-''' xr.where issue: copying a mask from a NetCDF file '''
+""" xr.where issue: copying a mask from a NetCDF file """
+# Copy this file by doing ...
+# The NetCDF files are stored in
+# ...
+import xarray as xr
+# Read wind file
+f = xr.open_dataset('CCSM4_ens1_19821201_19831130_ws10_0_NAtl_DJFmean.nc')
+lat = f.lat
+lon = f.lon
+ws10 = f.ws10
+print(ws10) # To see data
+# Read wave file
+f = xr.open_dataset('www.Hs.mask.nc')
+hs = f.hs
+hs = hs.rename({'latitude': 'lat', 'longitude': 'lon'})
+hs.coords['lon'] = lon
+print(hs) # To see nans
+# Try and set nans in the wind file
+ws10_masked = ws10.where(hs.isnull())
+print(ws10_masked)
+
